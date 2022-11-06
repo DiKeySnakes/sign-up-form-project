83339d2e2c0eb70b4ccfb3db9070aecaaaf15288
DiKeySnakes <DiKeySnakes@gmail.com>
2022-11-06 14:41:57 +0200

    Add compare passwords function. Modify index.html script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+const x = document.getElementById('password');
+const y = document.getElementById('confirm-password');
+const form = document.getElementById('form');
+
 function changeTypeX() {
-  const x = document.getElementById('password');
   if (x.type === 'password') {
     x.type = 'text';
   } else {
@@ -8,10 +11,16 @@ function changeTypeX() {
 }
 
 function changeTypeY() {
-  const y = document.getElementById('confirm-password');
   if (y.type === 'password') {
     y.type = 'text';
   } else {
     y.type = 'password';
   }
 }
+
+form.addEventListener('submit', (e) => {
+  if (x.value !== y.value) {
+    e.preventDefault();
+    alert('Your passwords do not match!');
+  }
+});
